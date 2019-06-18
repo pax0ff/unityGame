0df2060bff137983318b68397219e80f187c8689
Smarandache Stefan <stefan.smarandache96@gmail.com>
2019-06-18 16:14:11 +0300

    Upload file to GItHub
    Upload file to GItHub

---
+++ Assets/Scripts/PlayerJumping.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerJumping : MonoBehaviour
+{
+    public float jumpForce = 10f;
+
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public bool grounded = false;
+    public LayerMask whatIsGround;
+    private Rigidbody2D rigidBody;
+
+   
+
+    void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool jump = Input.GetButtonDown("Jump");
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        if (jump && grounded) GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpForce);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private float movement = 0f;
+    private Rigidbody2D rigidBody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+     void Update()
+     {
+        movement = Input.GetAxis("Horizontal");
+        if (movement > 0f)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+        }
+        else if (movement < 0f)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+        }
+    }
+ }
+ 
+
+   
